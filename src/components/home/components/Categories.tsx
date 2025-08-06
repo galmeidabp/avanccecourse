0f25412714eb3ac3b656cb732d339b3474f198ef
Gabriella Almeida <galmeidabp@gmail.com>
2025-08-06 09:21:10 -0300

    categories update

--- src/components/home/components/Categories.tsx
+++ src/components/home/components/Categories.tsx
@@ -5,7 +5,7 @@ export function Categories() {
   return (
     < div data-aos="fade-down" className="max-w-sm m-auto flex flex-col justify-center items-center md:items-start  mb-8 md:max-w-xl lg:max-w-5xl" >
       <h2 className="text-3xl font-bold mt-25">Explore por <br /> categorias</h2>
-      <div className="grid grid-cols-1 mt-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 lg:gap-5 md:mt-10">
+      <div className="grid grid-cols-2 mt-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 lg:gap-5 md:mt-10">
         <CategoriesCard title="Segurança avançada" Icon={LockKeyholeOpen} iconColor="text-amber-400" bgColor="bg-amber-200" />
         <CategoriesCard title={`Boa\nsemântica`} Icon={Scan} iconColor="text-red-400" bgColor="bg-red-200" />
         <CategoriesCard title="Componentes separados" Icon={Droplet} iconColor="text-cyan-400" bgColor="bg-cyan-200" cardBg="bg-gray-100" textCard="text-gray-900" />
